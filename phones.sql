ef260db08689c18dd2348e68120555d06e9c8ded
vadias <ra9ufy@mail.ru>
2023-09-11 22:08:21 +0700

    Add files via upload

---
+++ phones.sql
@@ -0,0 +1,25 @@
+SELECT * FROM gb.phones;
+
+SELECT * FROM gb.phones
+WHERE product_count > 2;
+
+SELECT * FROM gb.phones
+WHERE manufacturer = "Sumsung";
+
+SELECT * FROM gb.phones
+WHERE product_name  RLIKE "iphone";
+
+SELECT * FROM gb.phones
+WHERE  product_name  RLIKE "Sumsung";
+
+SELECT * FROM gb.phones
+WHERE  product_name  RLIKE '(1|2|3|4|5|6|7|8|9|0)';
+
+SELECT * FROM gb.phones
+WHERE  product_name  RLIKE '(8)';
+
+
+
+
+
+
